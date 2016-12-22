8cf831d233705c90cd78c7578456720d6552f52a
root <root@ubuntu-1gb-sfo1-01.localdomain>
2016-12-22 02:07:25 +0000

    daily backup

--- backup.sh
+++ backup.sh
@@ -7,13 +7,18 @@
 # 0 12 * * * /path/to/minecraft/backup.sh > /dev/null 2>&1
 
 CWD=$(readlink -f `dirname $0`)
-screen -S minecraft -X stuff "say [Backup] Server temporarily stops for backup in 60 seconds^M"
+screen -S minecraft -X stuff "say [Backup] Server goes volatile for backup in 10 seconds^M"
 sleep 10
+screen -S minecraft -X stuff "say [Backup] World is now volatile. Things may not be auto-saved for a short moment.^M"
 screen -S minecraft -X stuff "save-off^M"
 screen -S minecraft -X stuff "save-all^M"
+sleep 5
 cd $CWD
 git add . 
 git commit -a -m"daily backup"
+screen -S minecraft -X stuff "say [Backup] World is auto-saved.^M"
 screen -S minecraft -X stuff "save-on^M"
 screen -S minecraft -X stuff "say [Backup] Done...^M"
+git push origin master
+
 
